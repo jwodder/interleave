fa83b13b498d4a12f89ff203ab633e02d00fe543
John T. Wodder II <git@varonathe.org>
2022-01-14 12:45:23 -0500

    Test timing between yields

--- test/test_interleave.py
+++ test/test_interleave.py
@@ -1,4 +1,5 @@
-from time import sleep
+from math import isclose
+from time import monotonic, sleep
 from typing import Iterator, Sequence, Tuple
 from interleave import interleave
 
@@ -11,15 +12,20 @@ def sleeper(tid: int, delays: Sequence[int]) -> Iterator[Tuple[int, int]]:
         yield (tid, i)
 
 
-def test_simple_ordering() -> None:
+def test_simple() -> None:
     INTERVALS = [
         (0, 1, 2),
         (2, 2, 2),
         (5, 2, 1),
     ]
-    assert list(
+    woven = []
+    start = monotonic()
+    for i, x in enumerate(
         interleave([sleeper(i, intervals) for i, intervals in enumerate(INTERVALS)])
-    ) == [
+    ):
+        assert isclose(monotonic() - start, i * UNIT, rel_tol=0.1, abs_tol=0.1)
+        woven.append(x)
+    assert woven == [
         (0, 0),
         (0, 1),
         (1, 0),

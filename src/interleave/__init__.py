9899077f4f949b4b0e749433b6bd2a799df56c2c
John T. Wodder II <git@varonathe.org>
2022-01-14 18:59:04 -0500

    Use a custom multiproducer queue class that tracks whether any producers are left

--- src/interleave/__init__.py
+++ src/interleave/__init__.py
@@ -6,8 +6,9 @@ Visit <https://github.com/jwodder/interleave> for more information.
 
 from __future__ import annotations
 from concurrent.futures import ThreadPoolExecutor
-from queue import Empty, Queue
+from queue import Queue, SimpleQueue
 import sys
+from threading import Lock
 from types import TracebackType
 from typing import (
     Any,
@@ -21,6 +22,11 @@ from typing import (
     cast,
 )
 
+if sys.version_info[:2] >= (3, 8):
+    from typing import Protocol
+else:
+    from typing_extensions import Protocol
+
 __version__ = "0.1.0.dev1"
 __author__ = "John Thorvald Wodder II"
 __author_email__ = "interleave@varonathe.org"
@@ -29,9 +35,9 @@ __url__ = "https://github.com/jwodder/interleave"
 
 __all__ = ["interleave"]
 
-T = TypeVar("T")
-
 ExcInfo = Tuple[Type[BaseException], BaseException, TracebackType]
+
+T = TypeVar("T")
 
 
 class Result(Generic[T]):
@@ -61,39 +67,106 @@ class Result(Generic[T]):
         return cls(exc_info=(etype, e, tb))
 
 
-def interleave(
-    iterators: Iterable[Iterator[T]],
-    *,
-    max_workers: Optional[int] = None,
-    queue_size: int = 0,
-    queue_wait: float = 1.0
-) -> Iterator[T]:
-    pool = ThreadPoolExecutor(max_workers=max_workers)
-    queue: Queue[Result[T]] = Queue(queue_size)
-    futures = []
+class QueueProto(Protocol, Generic[T]):
+    """
+    Protocol for the behavior shared by queue.Queue and queue.SimpleQueue that
+    is of relevance to this package
+    """
 
-    def run_iterator(it: Iterator[T]) -> None:
+    def get(self, block: bool = ..., timeout: Optional[float] = ...) -> T:
+        ...
+
+    def put(self, item: T, block: bool = ..., timeout: Optional[float] = ...) -> None:
+        ...
+
+
+class EndOfInputError(Exception):
+    pass
+
+
+class FunnelQueue(Generic[T]):
+    def __init__(self, queue_size: Optional[int] = None) -> None:
+        self.queue: QueueProto[T]
+        if queue_size is None:
+            self.queue = SimpleQueue()
+        else:
+            self.queue = Queue(queue_size)
+        self.producer_qty = 0
+        self.lock = Lock()
+        self.done_sentinel = object()
+
+    def _acquire_producer(self) -> None:
+        with self.lock:
+            self.producer_qty += 1
+
+    def _release_producer(self) -> None:
+        with self.lock:
+            self.producer_qty -= 1
+            if self.producer_qty == 0:
+                self._put(cast(T, self.done_sentinel))
+
+    def _put(self, value: T) -> None:
+        self.queue.put(value)
+
+    def putter(self) -> FunnelPutter[T]:
+        return FunnelPutter(self)
+
+    def get(self) -> T:
+        x = self.queue.get()
+        if x is self.done_sentinel:
+            raise EndOfInputError()
+        else:
+            return x
+
+
+class FunnelPutter(Generic[T]):
+    def __init__(self, funnel: FunnelQueue[T]) -> None:
+        self.funnel = funnel
+
+    def __enter__(self) -> FunnelPutter[T]:
+        self.funnel._acquire_producer()
+        return self
+
+    def __exit__(
+        self,
+        _exc_type: Optional[Type[BaseException]],
+        _exc_val: Optional[BaseException],
+        _exc_tb: Optional[TracebackType],
+    ) -> None:
+        self.funnel._release_producer()
+
+    def __call__(self, value: T) -> None:
+        self.funnel._put(value)
+
+
+def funnel_iterator(funnel: FunnelQueue[Result[T]], it: Iterator[T]) -> None:
+    with funnel.putter() as put:
         while True:
             try:
                 x = next(it)
             except StopIteration:
                 return
             except BaseException:
-                queue.put(Result.for_exc())
+                put(Result.for_exc())
                 return
             else:
-                queue.put(Result(x))
+                put(Result(x))
 
+
+def interleave(
+    iterators: Iterable[Iterator[T]],
+    *,
+    max_workers: Optional[int] = None,
+    queue_size: Optional[int] = None,
+) -> Iterator[T]:
+    pool = ThreadPoolExecutor(max_workers=max_workers)
+    funnel: FunnelQueue[Result[T]] = FunnelQueue(queue_size)
     for it in iterators:
-        futures.append(pool.submit(run_iterator, it))
-
+        pool.submit(funnel_iterator, funnel, it)
     while True:
         try:
-            # We need to set a timeout so that keyboard interrupts aren't
-            # ignored on Windows
-            r = queue.get(timeout=queue_wait)
-        except Empty:
-            if all(f.done() for f in futures):
-                break
+            r = funnel.get()
+        except EndOfInputError:
+            break
         else:
             yield r.get()

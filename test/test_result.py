9c071e47d7e2dca46df1141e4885515b2af7ff0d
John T. Wodder II <git@varonathe.org>
2022-01-15 18:07:51 -0500

    More tests of Result

--- test/test_result.py
+++ test/test_result.py
@@ -1,5 +1,19 @@
+from typing import Optional
 import pytest
 from interleave import Result
+
+
+def test_successful() -> None:
+    xs = [1, 2, 3]
+    r = Result(xs)
+    assert r.success
+    assert r.get() is xs
+
+
+def test_none_value() -> None:
+    r: Result[Optional[int]] = Result(None)
+    assert r.success
+    assert r.get() is None
 
 
 def test_reraising() -> None:
@@ -7,6 +21,13 @@ def test_reraising() -> None:
         raise ValueError("Something went wrong")
     except BaseException:
         r = Result.for_exc()
+    assert not r.success
     with pytest.raises(ValueError) as excinfo:
         r.get()
     assert str(excinfo.value) == "Something went wrong"
+
+
+def test_not_for_exc() -> None:
+    with pytest.raises(ValueError) as excinfo:
+        Result.for_exc()
+    assert str(excinfo.value) == "No exception currently being handled"

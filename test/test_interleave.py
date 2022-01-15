0e75bee2f55759068d4de3cc35bcf18637d48e89
John T. Wodder II <git@varonathe.org>
2022-01-15 13:57:53 -0500

    Isolate the timing tests in a single test

--- test/test_interleave.py
+++ test/test_interleave.py
@@ -8,10 +8,6 @@ from interleave import interleave
 UNIT = 0.5
 
 
-def close2unit(f: float) -> bool:
-    return isclose(f, UNIT, rel_tol=0.3, abs_tol=0.1)
-
-
 def sleeper(tid: int, delays: Sequence[Union[int, str]]) -> Iterator[Tuple[int, int]]:
     for i, d in enumerate(delays):
         if isinstance(d, int):
@@ -21,22 +17,15 @@ def sleeper(tid: int, delays: Sequence[Union[int, str]]) -> Iterator[Tuple[int, 
             raise RuntimeError(d)
 
 
-@pytest.mark.flaky(reruns=5, condition="CI" in os.environ)
 def test_simple() -> None:
     INTERVALS = [
         (0, 1, 2),
         (2, 2, 2),
         (5, 2, 1),
     ]
-    woven = []
-    last_yield: Optional[float] = None
-    for x in interleave(sleeper(i, intervals) for i, intervals in enumerate(INTERVALS)):
-        this_yield = monotonic()
-        if last_yield is not None:
-            assert close2unit(this_yield - last_yield)
-        last_yield = this_yield
-        woven.append(x)
-    assert woven == [
+    assert list(
+        interleave(sleeper(i, intervals) for i, intervals in enumerate(INTERVALS))
+    ) == [
         (0, 0),
         (0, 1),
         (1, 0),
@@ -50,36 +39,32 @@ def test_simple() -> None:
 
 
 @pytest.mark.flaky(reruns=5, condition="CI" in os.environ)
+def test_simple_timing() -> None:
+    INTERVALS = [
+        (0, 1, 2),
+        (2, 2, 2),
+        (5, 2, 1),
+    ]
+    prev: Optional[float] = None
+    for _ in interleave(sleeper(i, intervals) for i, intervals in enumerate(INTERVALS)):
+        now = monotonic()
+        if prev is not None:
+            assert isclose(now - prev, UNIT, rel_tol=0.3, abs_tol=0.1)
+        prev = now
+
+
 def test_simple_error() -> None:
     INTERVALS: List[Tuple[Union[int, str], ...]] = [
         (0, 1, 2),
         (2, 2, "This is an error.", "This is not raised."),
         (5, "This is not seen.", 1),
     ]
-    expected = [
-        (0, 0),
-        (0, 1),
-        (1, 0),
-        (0, 2),
-        (1, 1),
-    ]
-    last_yield: Optional[float] = None
-    i = 0
     it = interleave(sleeper(i, intervals) for i, intervals in enumerate(INTERVALS))
-    while True:
-        if i < len(expected):
-            x = next(it)
-            assert x == expected[i]
-            this_yield = monotonic()
-            if last_yield is not None:
-                assert close2unit(this_yield - last_yield)
-            last_yield = this_yield
-            i += 1
-        else:
-            with pytest.raises(RuntimeError) as excinfo:
-                next(it)
-            assert str(excinfo.value) == "This is an error."
-            break
+    for expected in [(0, 0), (0, 1), (1, 0), (0, 2), (1, 1)]:
+        assert next(it) == expected
+    with pytest.raises(RuntimeError) as excinfo:
+        next(it)
+    assert str(excinfo.value) == "This is an error."
 
 
 def test_no_iterators() -> None:

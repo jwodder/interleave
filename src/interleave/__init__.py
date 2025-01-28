1542a54e741d22e4d3a8e32797fb2601cac9f1c9
John T. Wodder II <git@varonathe.org>
2025-01-28 14:47:33 -0500

    v0.3.0 — lazy_interleave()
    - Added `lazy_interleave()` function

--- src/interleave/__init__.py
+++ src/interleave/__init__.py
@@ -20,7 +20,7 @@ from time import monotonic
 from types import TracebackType
 from typing import Any, Generic, NoReturn, Protocol, TypeVar, cast
 
-__version__ = "0.3.0.dev1"
+__version__ = "0.3.0"
 __author__ = "John Thorvald Wodder II"
 __author_email__ = "interleave@varonathe.org"
 __license__ = "MIT"
